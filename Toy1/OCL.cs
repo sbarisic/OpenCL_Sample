4cc36d86045ab7f546ddd7208bd06910dcd404ce
Saša Barišić <cartmanium@gmail.com>
2025-12-29 14:08:50 +0100

    Add project files.

---
+++ Toy1/OCL.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Silk.NET.OpenCL;
+
+namespace Toy1
+{
+    unsafe class CLKernel
+    {
+        public nint kernel;
+
+        public CLKernel(CLContext Ctx, CLProgram Prog, string func_name)
+        {
+            kernel = Ctx.cL.CreateKernel(Prog.program, func_name, null);
+
+            if (kernel == IntPtr.Zero)
+            {
+                throw new Exception("Failed to create kernel");
+            }
+        }
+    }
+
+    unsafe class CLProgram
+    {
+        public nint program;
+
+        public CLProgram(CLContext Ctx, CLCommandQueue CQueue, string fileName)
+        {
+            if (!File.Exists(fileName))
+                throw new Exception($"File does not exist: {fileName}");
+
+
+            using StreamReader sr = new StreamReader(fileName);
+            string clStr = sr.ReadToEnd();
+
+            program = Ctx.cL.CreateProgramWithSource(Ctx.context, 1, new string[] { clStr }, null, null);
+            if (program == IntPtr.Zero)
+                throw new Exception("Failed to create CL program from source.");
+
+            var errNum = Ctx.cL.BuildProgram(program, 0, null, (byte*)null, null, null);
+
+            if (errNum != (int)ErrorCodes.Success)
+            {
+                _ = Ctx.cL.GetProgramBuildInfo(program, CQueue.device, ProgramBuildInfo.BuildLog, 0, null, out nuint buildLogSize);
+                byte[] log = new byte[buildLogSize / (nuint)sizeof(byte)];
+
+                fixed (void* pValue = log)
+                {
+                    Ctx.cL.GetProgramBuildInfo(program, CQueue.device, ProgramBuildInfo.BuildLog, buildLogSize, pValue, null);
+                }
+
+                string? build_log = Encoding.UTF8.GetString(log);
+
+                //Console.WriteLine("Error in kernel: ");
+                Console.WriteLine("=============== OpenCL Program Build Info ================");
+                Console.WriteLine(build_log);
+                Console.WriteLine("==========================================================");
+
+                Ctx.cL.ReleaseProgram(program);
+                throw new Exception("Exception!");
+            }
+        }
+    }
+
+    unsafe class CLCommandQueue
+    {
+        public nint device;
+        public nint commandQueue;
+
+        public CLCommandQueue(CLContext Ctx)
+        {
+            int errNum = Ctx.cL.GetContextInfo(Ctx.context, ContextInfo.Devices, 0, null, out nuint deviceBufferSize);
+            if (errNum != (int)ErrorCodes.Success)
+                throw new Exception("Failed call to clGetContextInfo(...,GL_CONTEXT_DEVICES,...)");
+
+            if (deviceBufferSize <= 0)
+                throw new Exception("No devices available.");
+
+
+            nint[] devices = new nint[deviceBufferSize / (nuint)sizeof(nuint)];
+            fixed (void* pValue = devices)
+            {
+                int er = Ctx.cL.GetContextInfo(Ctx.context, ContextInfo.Devices, deviceBufferSize, pValue, null);
+
+            }
+
+            if (errNum != (int)ErrorCodes.Success)
+            {
+                throw new Exception("Failed to get device IDs");
+            }
+
+            // In this example, we just choose the first available device.  In a
+            // real program, you would likely use all available devices or choose
+            // the highest performance device based on OpenCL device queries
+            commandQueue = Ctx.cL.CreateCommandQueue(Ctx.context, devices[0], CommandQueueProperties.None, null);
+
+            if (commandQueue == IntPtr.Zero)
+                throw new Exception("Failed to create commandQueue for device 0");
+
+            device = devices[0];
+        }
+    }
+
+    unsafe class CLContext
+    {
+        public CL cL;
+        public nint context;
+
+        public CLContext()
+        {
+            cL = CL.GetApi();
+
+            var errNum = cL.GetPlatformIDs(1, out nint firstPlatformId, out uint numPlatforms);
+
+            if (errNum != (int)ErrorCodes.Success || numPlatforms <= 0)
+                throw new Exception("Failed to find any OpenCL platforms.");
+
+            // Next, create an OpenCL context on the platform.  Attempt to
+            // create a GPU-based context, and if that fails, try to create
+            // a CPU-based context.
+            nint[] contextProperties = new nint[]
+            {
+                (nint)ContextProperties.Platform,
+                firstPlatformId,
+                0
+            };
+
+            fixed (nint* p = contextProperties)
+            {
+                context = cL.CreateContextFromType(p, DeviceType.Gpu, null, null, out errNum);
+                if (errNum != (int)ErrorCodes.Success)
+                {
+                    Console.WriteLine("Could not create GPU context, trying CPU...");
+                    context = cL.CreateContextFromType(p, DeviceType.Cpu, null, null, out errNum);
+
+                    if (errNum != (int)ErrorCodes.Success)
+                    {
+                        throw new Exception("Failed to create an OpenCL GPU or CPU context.");
+                    }
+                }
+            }
+        }
+
+        public void AllocateObject()
+        {
+
+        }
+
+        static unsafe bool CreateMemObjects(CL cl, nint context, nint[] memObjects, float[] a, float[] b)
+        {
+            fixed (void* pa = a)
+            {
+                memObjects[0] = cl.CreateBuffer(context, MemFlags.ReadOnly | MemFlags.CopyHostPtr, sizeof(float) * ARRAY_SIZE, pa, null);
+            }
+
+            fixed (void* pb = b)
+            {
+                memObjects[1] = cl.CreateBuffer(context, MemFlags.ReadOnly | MemFlags.CopyHostPtr, sizeof(float) * ARRAY_SIZE, pb, null);
+            }
+
+            memObjects[2] = cl.CreateBuffer(context, MemFlags.ReadWrite, sizeof(float) * ARRAY_SIZE, null, null);
+
+            if (memObjects[0] == IntPtr.Zero || memObjects[1] == IntPtr.Zero || memObjects[2] == IntPtr.Zero)
+            {
+                Console.WriteLine("Error creating memory objects.");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ Toy1/Program.cs
@@ -0,0 +1,94 @@
+﻿using Silk.NET.OpenCL;
+
+namespace Toy1
+{
+    internal class Program
+    {
+        const int ARRAY_SIZE = 1000;
+
+        static unsafe void Main(string[] args)
+        {
+            // Create an OpenCL context on first available platform
+            CLContext Ctx = new CLContext();
+
+
+            // Create a command-queue on the first device available
+            // on the created context
+            CLCommandQueue CQueue = new CLCommandQueue(Ctx);
+
+
+            // Create OpenCL program from HelloWorld.cl kernel source
+            CLProgram Prog = new CLProgram(Ctx, CQueue, "data/kernel.cl");
+
+            // Create OpenCL kernel
+            CLKernel Krn = new CLKernel(Ctx, Prog, "hello_kernel");
+
+            // Create memory objects that will be used as arguments to
+            // kernel.  First create host memory arrays that will be
+            // used to store the arguments to the kernel
+            float[] result = new float[ARRAY_SIZE];
+            float[] a = new float[ARRAY_SIZE];
+            float[] b = new float[ARRAY_SIZE];
+            for (int i = 0; i < ARRAY_SIZE; i++)
+            {
+                a[i] = (float)i;
+                b[i] = (float)(i * 2);
+            }
+
+            nint[] memObjects = new nint[3];
+            if (!CreateMemObjects(cl, context, memObjects, a, b))
+            {
+                Cleanup(cl, context, commandQueue, program, kernel, memObjects);
+                return;
+            }
+
+            // Set the kernel arguments (result, a, b)
+            int errNum = cl.SetKernelArg(kernel, 0, (nuint)sizeof(nint), memObjects[0]);
+            errNum |= cl.SetKernelArg(kernel, 1, (nuint)sizeof(nint), memObjects[1]);
+            errNum |= cl.SetKernelArg(kernel, 2, (nuint)sizeof(nint), memObjects[2]);
+
+            if (errNum != (int)ErrorCodes.Success)
+            {
+                Console.WriteLine("Error setting kernel arguments.");
+                Cleanup(cl, context, commandQueue, program, kernel, memObjects);
+                return;
+            }
+
+            nuint[] globalWorkSize = new nuint[1] { ARRAY_SIZE };
+            nuint[] localWorkSize = new nuint[1] { 1 };
+
+            // Queue the kernel up for execution across the array
+            errNum = cl.EnqueueNdrangeKernel(commandQueue, kernel, 1, (nuint*)null, globalWorkSize, localWorkSize, 0, (nint*)null, (nint*)null);
+            if (errNum != (int)ErrorCodes.Success)
+            {
+                Console.WriteLine("Error queuing kernel for execution.");
+                Cleanup(cl, context, commandQueue, program, kernel, memObjects);
+                return;
+            }
+
+            fixed (void* pValue = result)
+            {
+                // Read the output buffer back to the Host
+                errNum = cl.EnqueueReadBuffer(commandQueue, memObjects[2], true, 0, ARRAY_SIZE * sizeof(float), pValue, 0, null, null);
+                if (errNum != (int)ErrorCodes.Success)
+                {
+                    Console.WriteLine("Error reading result buffer.");
+                    Cleanup(cl, context, commandQueue, program, kernel, memObjects);
+                    return;
+                }
+            }
+
+            // Output the result buffer
+            for (int i = 0; i < ARRAY_SIZE; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+            Console.WriteLine("Executed program succesfully.");
+            Cleanup(cl, context, commandQueue, program, kernel, memObjects);
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
